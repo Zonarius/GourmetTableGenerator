cc0e7d6fbc0cf5904a47573b27e137819b0d23a5
zonarius <zonarius90@gmail.com>
2017-05-15 20:03:21 +0200

    Make it possible to choose input dir as output dir

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,10 +9,13 @@ import { RenderData } from "./model";
 const renderTemplate = promisify(nunjucks.render);
 
 export async function render(config: Config, data: RenderData): Promise<void> {
+    const htmlDir = path.join(__dirname, '..', 'html');
     const output$: Promise<string> = renderTemplate(path.join(__dirname, '..', 'htmlsrc', 'index.nunjucks'), data);
 
     await fs.mkdirp(config.outputDir);
-    await fs.copy(path.join(__dirname, '..', 'html'), config.outputDir);
+    if (path.relative(config.outputDir, htmlDir) !== '') {
+        await fs.copy(htmlDir, config.outputDir);
+    }
     const raw$ = fs.writeFile(path.join(config.outputDir, 'raw.json'), JSON.stringify(data, undefined, 2));
     const output = await output$;
     const template$ = fs.writeFile(path.join(config.outputDir, 'index.html'), output);
